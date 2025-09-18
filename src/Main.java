6bc5f2e0d20a4cc1769d709fd8e74be23444a444
marcmoye <moyemarc24@gmail.com>
2025-09-18 13:20:56 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        double springCost = 100;
+        double SummerCost = 200;
+        double fallCost = 150;
+        double winterCost = 300;
+
+        double totalYearlyCost = springCost + SummerCost + fallCost + winterCost;
+        System.out.println("Total Yearly Cost: " + totalYearlyCost);
+    }
+}
